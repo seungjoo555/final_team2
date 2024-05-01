770ff6518dce8732f6d8da12544d8d6aafdc07ae
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2024-05-01 20:53:09 +0900

    헤더 정리, 헤더 강의 css완

--- spring/final_project/src/main/java/kr/kh/team2/controller/LectureController.java
+++ spring/final_project/src/main/java/kr/kh/team2/controller/LectureController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import kr.kh.team2.pagination.Criteria;
 import lombok.extern.log4j.Log4j;
 
 @Log4j
@@ -11,7 +12,7 @@ import lombok.extern.log4j.Log4j;
 public class LectureController {
 
 	@GetMapping("/lecture/list")
-	public String postList(Model model) {
+	public String postList(Model model, Criteria cri) {
 		model.addAttribute("title", "강의글 목록");
 		return "/lecture/list";
 	}
